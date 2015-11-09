e4671904b937f3cfd7017aacaffc1a59b5b87a3c
Leanne Northrop <leanne.northrop@gmail.com>
2015-11-09 22:51:06 +0000

    Inital commit of basic-minify with sourcemaps. Sourcemaps are generated using gulp-sourcemaps. Javascript are bundled using gulp-concat CSS are pre-processed using gulp-postcss which applies auto-prefixer and csswring at present.

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,40 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     size = require('gulp-size'),
     ghPages = require('gulp-gh-pages'),
-    srcDir = "src"
+    rename = require('gulp-rename'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    csswring = require('csswring'),
+    srcDir = "src",
     destDir = "./dist";
 
 
 gulp.task('styles', function() {
+  var processors = [
+        autoprefixer({browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']}),
+        csswring({
+          preserveHacks: true,
+          removeAllComments: true
+        })
+    ];
   gulp.src([srcDir+'/css/**/*.css'])
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest(destDir+'/css'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(postcss([csswring]))
+    .pipe(sourcemaps.write('./', {includeContent: false, sourceRoot: '../css'}))
     .pipe(gulp.dest(destDir+'/css'));
+
 });
 
 gulp.task('scripts', function() {
   return gulp.src([srcDir+'/js/**/*.js'])
+    .pipe(gulp.dest(destDir+'/js'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('./',{includeContent: true, sourceRoot: '../js'}))
     .pipe(gulp.dest(destDir+'/js'));
 });
 
