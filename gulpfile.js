e74b34b7ef66ffc51ed3e1957230f8cebe2debdc
Leanne Northrop <leanne.northrop@gmail.com>
2015-11-10 18:46:56 +0000

    Gathered paths into one object.

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,43 @@ var gulp = require('gulp'),
 var plugins = require('gulp-load-plugins')();
 
 var failOnError = true;
-    srcDir = "src",
-    destDir = "./dist";
+
+var basePaths = {
+    src: 'src',
+    dest: './dist',
+    bower: 'bower_components/'
+};
+
+var paths = {
+    images: {
+        src: basePaths.src + '/images',
+        dest: basePaths.dest + '/images'
+    },
+    scripts: {
+        src: basePaths.src + '/js',
+        dest: basePaths.dest + '/js'
+    },
+    sass: {
+        src: basePaths.src + '/sass',
+        dest: basePaths.dest + '/css'
+    },
+    styles: {
+        src: basePaths.src + '/css',
+        dest: basePaths.dest + '/css'
+    },    
+    sprite: {
+        src: basePaths.src + '/sprite/*'
+    },
+    fonts: {
+        src: basePaths.src + '/fonts',
+        dest: basePaths.dest + '/fonts'
+    },
+    html: {
+      src: basePaths.src,
+      dest: basePaths
+    } 
+};
+
 
 gulp.task('init', function() {
   failOnError = false;
@@ -43,23 +78,23 @@ gulp.task('styles', function() {
     return (file.relative + " (" + file.csslint.errorCount + " errors)\n" + errors);
   }
 
-  gulp.src([srcDir+'/css/**/*.css'])
+  gulp.src([paths.styles.src+'/**/*.css'])
     .pipe(plugins.csslint())
     .pipe(plugins.csslint.reporter())
     .pipe(plugins.if(failOnError, plugins.csslint.reporter('fail')))
     .pipe(plugins.notify(getMessages))    
-    .pipe(gulp.dest(destDir+'/css'))
+    .pipe(gulp.dest(paths.styles.dest))
     .pipe(plugins.sourcemaps.init({loadMaps: true}))
     .pipe(plugins.rename({suffix: '.min'}))
     .pipe(plugins.postcss([csswring]))
     .pipe(plugins.sourcemaps.write('./', {includeContent: false, sourceRoot: '../css'}))
-    .pipe(gulp.dest(destDir+'/css'));
+    .pipe(gulp.dest(paths.styles.dest));
 
 });
 
 gulp.task('scripts:clean', function() {
   try {
-    var file = srcDir+'/js/all.js';
+    var file = paths.scripts.src+'/all.js';
     var f = fs.statSync(file);  
     if (f.isFile()) {
       return del([file]);  
@@ -100,7 +135,7 @@ gulp.task('scripts:concat', function() {
       return (file.relative + " (" + count + " errors)\n" + errors);
     }
 
-    var stream = gulp.src(['!'+ srcDir + '/js/libs/**/*.*', '!'+ srcDir+'/all.js', srcDir+'/js/**/*.js'])
+    var stream = gulp.src(['!'+ paths.scripts.src + '/libs/**/*.*', '!'+ paths.scripts.src+'/all.js', paths.scripts.src+'/**/*.js'])
                       .pipe(plugins.plumber())                    // Prevent lint/check style errors to not fail pipeline here
                       .pipe(plugins.jshint('.jshintrc'))          // Lint   
                       .pipe(plugins.jscs())                       // Code style check
@@ -110,10 +145,10 @@ gulp.task('scripts:concat', function() {
                       .pipe(plugins.jscs.reporter())              // Report style check errors to console
                       .pipe(plugins.if(failOnError, plugins.jscs.reporter('fail')))   // Fail if required on style errors
                       .pipe(plugins.if(failOnError, plugins.jshint.reporter('fail'))) // Fail if required on lint errors
-                      .pipe(gulp.dest(destDir+'/js'))             // Copy files to dest
+                      .pipe(gulp.dest(paths.scripts.dest))             // Copy files to dest
                       .pipe(plugins.concat('all.js'))             // Concat all JS into All
-                      .pipe(gulp.dest(srcDir+'/js'))              // Write all to both dest and src (for min task)
-                      .pipe(gulp.dest(destDir+'/js'))
+                      .pipe(gulp.dest(paths.scripts.src))              // Write all to both dest and src (for min task)
+                      .pipe(gulp.dest(paths.scripts.dest))
                       .on('error', plugins.notify.onError(function (error) {
                         return error.message;
                       }));
@@ -121,21 +156,21 @@ gulp.task('scripts:concat', function() {
 });
 
 gulp.task('scripts:min', function() {
-  var stream = gulp.src(['!'+ srcDir + '/js/libs/**/*.*', srcDir+'/js/**/*.js'])
+  var stream = gulp.src(['!'+ paths.scripts.src + '/libs/**/*.*', paths.scripts.src+'/**/*.js'])
     .pipe(plugins.plumber({errorHandler:failOnError}))
     .pipe(plugins.sourcemaps.init({loadMaps: true}))    
     .pipe(plugins.uglify().on('error', gutil.log))
     .pipe(plugins.rename({suffix: '.min'}))
     .pipe(plugins.sourcemaps.write('./',{includeContent: false, sourceRoot: '../js'}))
     .pipe(plugins.plumber.stop())
-    .pipe(gulp.dest(destDir+'/js'));
+    .pipe(gulp.dest(paths.scripts.dest));
     return stream;
 });
 
 gulp.task('scripts:copy', function() {
-  var stream = gulp.src([srcDir + '/js/libs/**/*.*'])
+  var stream = gulp.src([paths.scripts.src + '/libs/**/*.*'])
     .pipe(plugins.sourcemaps.init({loadMaps: true}))
-    .pipe(gulp.dest(destDir+'/js/libs'));
+    .pipe(gulp.dest(paths.scripts.dest+'/libs'));
     return stream;
 });
 
@@ -144,40 +179,40 @@ gulp.task('scripts', function(cb){
 });
 
 gulp.task('fonts', function() {
-  return gulp.src([srcDir+'/fonts/**/*.*'])
-    .pipe(gulp.dest(destDir+'/fonts'));
+  return gulp.src([paths.fonts.src+'/**/*.*'])
+    .pipe(gulp.dest(paths.fonts.dest));
 });
 
 // Optimizes the images that exists
 gulp.task('images', function () {
-  return gulp.src(srcDir+'/images/**')
-    .pipe(plugins.changed(destDir+'/images'))
+  return gulp.src(paths.images.src+'/**')
+    .pipe(plugins.changed(paths.images.dest))
     .pipe(plugins.imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
       interlaced: true
     }))
-    .pipe(gulp.dest(destDir+'/images'))
+    .pipe(gulp.dest(paths.images.dest))
     .pipe(plugins.size({title: 'images'}));
 });
 
 gulp.task('html', function() {
-  gulp.src(srcDir+'/**/*.html')
-    .pipe(gulp.dest(destDir+'/'));
+  gulp.src(paths.html.src+'/**/*.html')
+    .pipe(gulp.dest(paths.html.dest+'/'));
 });
 
 gulp.task('browser-sync', ['styles', 'scripts'], function() {
   browserSync({
     server: {
-      baseDir: destDir + "/",
+      baseDir: basePaths.dest + "/",
       injectChanges: true // this is new
     }
   });
 });
 
 gulp.task('deploy', function() {
-  return gulp.src(destDir+'/**/*')
+  return gulp.src(basePaths.dest+'/**/*')
              .pipe(plugins.ghPages())
              .pipe(plugins.notify("Deployed to github pages"));
 });
@@ -192,40 +227,39 @@ gulp.task('watch', function() {
   }  
 
   function changeEvent(evt) {
-      gutil.log('File', gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + srcDir+ ')/'), '')), 'was', gutil.colors.magenta(evt.type));
+      gutil.log('File', gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + basePaths.src+ ')/'), '')), 'was', gutil.colors.magenta(evt.type));
   }
 
   // Watch .html files
-  gulp.watch(srcDir+'/**/*.html', ['html', browserSync.reload]).on('change', function(evt) {
+  gulp.watch(paths.html.src+'/**/*.html', ['html', browserSync.reload]).on('change', function(evt) {
         changeEvent(evt);
     });
-  gulp.watch(destDir+"/*.html").on('change', browserSync.reload);
+  gulp.watch(paths.html.dest+"/*.html").on('change', browserSync.reload);
   // Watch .css files
-  gulp.watch(srcDir+'/css/**/*.css', ['styles', browserSync.reload])
+  gulp.watch(paths.html.src+'/css/**/*.css', ['styles', browserSync.reload])
       .on('error', swallowError)
       .on('change', function(evt) {
           changeEvent(evt);
       });
   // Watch .js files
-  gulp.watch(srcDir+'/js/**/*.js', ['scripts', browserSync.reload])
+  gulp.watch(paths.scripts.src+'/**/*.js', ['scripts', browserSync.reload])
       .on('error', swallowError)
       .on('change', function(evt) {
           changeEvent(evt);
       });
   // Watch image files
-  gulp.watch(srcDir+'/images/**/*', ['images', browserSync.reload])
+  gulp.watch(paths.images.src+'/**/*', ['images', browserSync.reload])
       .on('change', function(evt) {
           changeEvent(evt);
       });
-  gulp.watch(destDir+'/images/**/*', ['change', browserSync.reload])
+  gulp.watch(paths.images.dest+'/**/*', ['change', browserSync.reload])
       .on('change', function(evt) {
           changeEvent(evt);
       });  
 });
 
 gulp.task('clean', ['scripts:clean'], function() {
-    del([srcDir+'/js/all.js']);
-    return del([destDir+'/css', destDir+'/js', destDir+'/images', destDir+'/fonts', destDir+"/**/*.html"]);
+    return del([paths.styles.dest, paths.scripts.dest, paths.images.dest, paths.fonts.dest, paths.html.dest+"/**/*.html"]);
 });
 
 gulp.task('serve', function() { 
